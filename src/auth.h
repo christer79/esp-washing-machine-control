828d0546616523bfa1a15272f7675d9a162d142f
Christer Eriksson <christer.eriksson@jeppesen.com>
2023-12-26 09:34:59 +0100

    PubSubClient example with mqtt auth

---
+++ src/auth.h
@@ -0,0 +1,6 @@
+const char *ssid = "........";
+const char *password = "........";
+const char *mqtt_server = "broker.mqtt-dashboard.com";
+
+const char *mqtt_user = "mqtt";
+const char *mqtt_password = "....";
